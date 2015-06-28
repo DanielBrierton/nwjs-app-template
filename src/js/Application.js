3d677a59cda9fd208214303cbf6ab5367453a746
Daniel Brierton <daniel@brierton.me>
2015-06-28 21:26:52 +0100

    Added example of using nodeRequire to show importing node modules

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,7 +1,11 @@
 define(function () {
+    var fs = nodeRequire('fs');
+
     var Application = function (options) {
-        options.container.innerHTML = '<h1>Hello World!</h1>'
-            + 'We are using NW.js v' + process.versions['node-webkit'] + '.';
+        var fileContents = fs.readFileSync('index.html').toString().replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+        options.container.innerHTML = '<h1>index.html\'s content, read by fs: </h1>'
+            + '<textarea style="width:500px;height:200px;">' + fileContents + '</textarea>'
+            + '<p>We are using NW.js v' + process.versions['node-webkit'] + '.</p>';
     };
 
     return Application;
